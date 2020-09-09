287ad0d397a234b4bf069f7ef46743c95beb4a0d
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-09-09 12:19:01 +0900

    既読機能フロント記述完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,6 @@ class PostsController < ApplicationController
    Post.create(content: params[:content])
    redirect_to action: :index
   end
-
   def checked
     post = Post.find(params[:id])
     if post.checked 
